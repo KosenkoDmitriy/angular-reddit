1a043f6fca74da49754381445426df563741cc84
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2017-12-22 16:10:31 +0300

    Second small refactoring

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -8,4 +8,23 @@ export class Article {
     this.link = link;
     this.votes = votes || 0;
   }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  // domain() is a utility function that extracts
+  // the domain from a URL, which we will explain shortly
+  domain(): string {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (err) {
+      return null;
+    }
+  }
 }
